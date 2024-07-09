e69288a4b72a7d645c45b1e7d407c64e0919a7e8
Dick Knol <dgknol78@gmail.com>
2024-07-09 13:45:30 +0200

    gateway

---
+++ mail_gateway/hooks.py
@@ -0,0 +1,13 @@
+def pre_init_hook(env):
+    """
+    The objective of this hook is to speed up the installation
+    of the module on an existing Odoo instance.
+
+    Without this script, big databases can take a long time to install this
+    module.
+    """
+    return
+    env.cr.execute(
+        """ALTER TABLE mail_message
+    ADD COLUMN gateway_channel_id int"""
+    )
